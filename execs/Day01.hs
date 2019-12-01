3d4cfb4d6f3cad507316b5eef1c52a42cef9ae5c
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-01 11:09:22 +0100

    🛠 cabal: init the repository
    … or as shapr would say, make a cabbage 🥬, a cabal package

--- execs/Day01.hs
+++ execs/Day01.hs
@@ -1,7 +1,7 @@
 module Main where
 
 main =
-  do masses <- map (read :: String -> Integer) . lines <$> readFile "input.txt"
+  do masses <- map (read :: String -> Integer) . lines <$> readFile "inputs/input01.txt"
      print $ sum . map fuel $ masses
      print $ sum . map (sum . takeWhile (>0) . iterate fuel) . map fuel $ masses
 
