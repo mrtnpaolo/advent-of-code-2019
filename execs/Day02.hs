a592c3c4e0af1de2f83c2bffc0890b9cdc901072
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-02 06:58:29 +0100

    🎄 day-02: part 2

--- execs/Day02.hs
+++ execs/Day02.hs
@@ -12,14 +12,20 @@ import qualified Data.Map.Strict as M
 main =
   do raw <- map (read :: String -> Int) . lines . map c2n . init <$> getRawInput 2
      let ops = [ eval xs | xs <- chunksOf 4 raw ]
-     mapM_ print ops
+     -- mapM_ print ops
      let mem = alarm $ M.fromAscList $ zip [0..] raw
-     pure $! traceShowId $! mem
-     print $ run mem 0
+     -- pure $! traceShowId $! mem
+     -- print $ run mem 0 M.! 0
+     let space = [ ((noun, verb), run (setNoun noun . setVerb verb $ mem) 0)
+                 | noun <- [0..99], verb <- [0..99] ]
+     let solns = filter ((19690720 ==) . snd) space
+     mapM_ print solns
   where
     c2n ',' = '\n'
     c2n  x  =  x
-    alarm = M.insert 2 2 . M.insert 1 12
+    alarm = setVerb 2 . setNoun 12
+    setNoun = M.insert 1
+    setVerb = M.insert 2
 
 data IntCode = Halt | Sum Int Int Int | Mul Int Int Int | Noop
   deriving (Show)
@@ -45,8 +51,8 @@ step m i =
                            b = m M.! b'
     Noop        -> {- traceShowId $! -} (m,True)
 
-run :: Mem -> Int {- ^ instruction pointer -} -> Mem
-run m i = go m i False
+run :: Mem -> Int {- ^ instruction pointer -} -> Int
+run m i = go m i False M.! 0
   where
     go m _ True = m
     go m i False = let (m',done) = step m i in go m' (i+4) done
