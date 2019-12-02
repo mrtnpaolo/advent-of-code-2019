8530c1f2140f52cf97e8ae004ed7d566bc41b7f5
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-02 08:20:54 +0100

    🏋️‍♀️ day-02: refactor
    • Hoisted the instruction fetching and memory setup up one level
    • Simpler step function
    • Switched memory model from Map to IntMap
    • Simplify input parse code
    • Explicit `part1`/`part2`
    • Fixed `-Wall` warnings (except shadowing)

--- execs/Day02.hs
+++ execs/Day02.hs
@@ -2,57 +2,76 @@ module Main where
 
 import Advent
 
-import Debug.Trace
-
 import Data.List.Split
 
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
+import Data.IntMap.Strict (IntMap, (!))
+import qualified Data.IntMap.Strict as M
 
+main :: IO ()
 main =
-  do raw <- map (read :: String -> Int) . lines . map c2n . init <$> getRawInput 2
-     let ops = [ eval xs | xs <- chunksOf 4 raw ]
-     -- mapM_ print ops
-     let mem = alarm $ M.fromAscList $ zip [0..] raw
-     -- pure $! traceShowId $! mem
-     -- print $ run mem 0 M.! 0
-     let space = [ ((noun, verb), run (setNoun noun . setVerb verb $ mem) 0)
-                 | noun <- [0..99], verb <- [0..99] ]
-     let solns = filter ((19690720 ==) . snd) space
-     mapM_ print solns
+  do raw <- map (read :: String -> Int) . words . map sep <$> getRawInput 2
+     -- print $ (length raw, length raw `divMod` 4)
+     -- print raw
+     -- dumpProgram raw
+     let mem = fromInts raw
+     print $ part1 mem
+     print `mapM_` part2 mem
   where
-    c2n ',' = '\n'
-    c2n  x  =  x
-    alarm = setVerb 2 . setNoun 12
-    setNoun = M.insert 1
-    setVerb = M.insert 2
+    sep ',' = ' '
+    sep  x  =  x
 
-data IntCode = Halt | Sum Int Int Int | Mul Int Int Int | Noop
+part1 :: Mem -> Int
+part1 = run . setup 12 2
+
+part2 :: Mem -> [Int]
+part2 mem
+  = [ 100 * noun + verb | noun <- [0..99]
+                        , verb <- [0..99]
+                        , run (setup noun verb mem) == 19690720 ]
+
+dumpProgram :: [Int] -> IO ()
+dumpProgram raw = mapM_ print ops
+  where
+    ops = [ fromOpcode xs | xs <- chunksOf 4 raw ]
+
+data IntCode
+  = Halt
+  | Sum Int Int Int
+  | Mul Int Int Int
+  | Unk
   deriving (Show)
 
-eval (99:_) = Halt
-eval [ 1,a,b,c] = Sum a b c
-eval [ 2,a,b,c] = Mul a b c
-eval _          = Noop
+fromOpcode :: [Int] -> IntCode
+fromOpcode (99:_    ) = Halt
+fromOpcode [ 1,a,b,c] = Sum a b c
+fromOpcode [ 2,a,b,c] = Mul a b c
+fromOpcode _          = Unk
 
-type Mem = M.Map Int Int
+type Mem = IntMap Int
 
-step :: Mem
-     -> Int {- ^ instruction pointer -}
-     -> (Mem,Bool) {- ^ (new memory, done) -}
-step m i =
-  case eval (take 4 . drop i . M.elems $ m) of
-    Halt        -> {- traceShowId $! -} (m,True)
-    Sum a' b' c -> {- traceShowId $! -} (M.insert c (a+b) m,False)
-                     where a = m M.! a'
-                           b = m M.! b'
-    Mul a' b' c -> {- traceShowId $! -} (M.insert c (a*b) m,False)
-                     where a = m M.! a'
-                           b = m M.! b'
-    Noop        -> {- traceShowId $! -} (m,True)
+fromInts :: [Int] -> Mem
+fromInts = M.fromAscList . zip [0..]
 
-run :: Mem -> Int {- ^ instruction pointer -} -> Int
-run m i = go m i False M.! 0
+step :: Mem -> IntCode -> Mem
+step m (Sum a b c) = M.insert c (m!a + m!b) m
+step m (Mul a b c) = M.insert c (m!a * m!b) m
+step m Unk         = m                        -- unknown, unclear
+step _ Halt        = error "executing Halt"
+
+run :: Mem -> Int {- ^ value of memory location 0 at the end of the program -}
+run m
+  = go m 0 (fetch 0 m)
   where
-    go m _ True = m
-    go m i False = let (m',done) = step m i in go m' (i+4) done
+    go m _ Halt = m ! 0
+    go m i op   = let m' = step m op
+                      i' = i+4
+                  in go m' i' (fetch i' m)
+
+    fetch :: Int -> Mem -> IntCode
+    fetch i = fromOpcode . take 4 . drop i . M.elems
+
+setup :: Int {- ^ noun -} -> Int {- ^ verb -} -> Mem -> Mem
+setup noun verb = setNoun noun . setVerb verb
+  where
+    setNoun = M.insert 1
+    setVerb = M.insert 2
