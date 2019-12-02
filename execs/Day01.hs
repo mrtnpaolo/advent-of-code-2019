7676f65cad8ed7a7acce8961428aafe1095557d1
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-02 09:50:51 +0100

    🏋️‍♀️ day-01: refactor and fix potential bug
    The naive `fuel` implementation underflows on 0..5:
    
        *Main> map (subtract 2 . (`div` 3)) [0..6]
        [-2,-2,-2,-1,-1,-1,0]
    
    Breaking `part1` inputs involving small masses.

--- execs/Day01.hs
+++ execs/Day01.hs
@@ -2,9 +2,17 @@ module Main (main) where
 
 import Advent
 
+main :: IO ()
 main =
   do masses <- map (read :: String -> Integer) . lines <$> getRawInput 1
-     print $ sum . map fuel $ masses
-     print $ sum . map (sum . takeWhile (>0) . iterate fuel) . map fuel $ masses
+     print $ part1 masses
+     print $ part2 masses
 
-fuel = subtract 2 . (`div` 3)
+part1 :: Integral a => [a] -> a
+part1 = sum . map fuel
+
+part2 :: Integral a => [a] -> a
+part2 = sum . map (sum . takeWhile (>0) . iterate fuel) . map fuel
+
+fuel :: Integral a => a -> a
+fuel = max 0 . subtract 2 . (`div` 3)
