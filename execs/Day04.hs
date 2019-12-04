c66888998d1867b2b64c9ee2778abe4268bdf540
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-04 07:17:25 +0100

    🛠 day-04: more refactoring

--- execs/Day04.hs
+++ execs/Day04.hs
@@ -8,26 +8,28 @@ import qualified Data.Map.Strict as M
 main :: IO ()
 main =
   do [lower,higher] <- map read . words . map sep <$> getRawInput 4
-     print $ part1 [lower..higher]
-     print $ part2 [lower..higher]
+     print $ satisfying part1 [lower..higher]
+     print $ satisfying part2 [lower..higher]
   where
     sep '-' = ' '
     sep x   = x
 
-part1 :: [Int] -> Int
-part1 = length . filter (\x -> and $ pure x <**> criterions) . map show
-  where
-    criterions =
-      [ any (uncurry (==)) . adjacents
-      , all (uncurry (<=)) . adjacents ]
+type Criterion = String -> Bool
 
-part2 :: [Int] -> Int
-part2 = length . filter (\x -> and $ pure x <**> criterions) . map show
-  where
-    criterions =
-      [ all (uncurry (<=)) . adjacents
-      , not . M.null . M.filter ([2] ==) . M.fromListWith (++) . map (second pure) . rle ]
+satisfying :: [Criterion] -> [Int] -> Int
+satisfying cs = length . filter (\x -> and $ pure (show x) <**> cs)
 
+part1 :: [Criterion]
+part1 =
+  [ any (uncurry (==)) . adjacents
+  , all (uncurry (<=)) . adjacents ]
+
+part2 :: [Criterion]
+part2 =
+  [ all (uncurry (<=)) . adjacents
+  , not . M.null . M.filter ([2] ==) . M.fromListWith (++) . map (second pure) . rle ]
+
+-- | List of pairs of adjacent elements
 adjacents :: [a] -> [(a,a)]
 adjacents = tail <**> zip
 
