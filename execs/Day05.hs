6bcf8d113fff06a1dd984f0c67d71b23c52107eb
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-05 08:18:40 +0100

    🎄 day-05: part 2

--- execs/Day05.hs
+++ execs/Day05.hs
@@ -25,8 +25,8 @@ data IntCode
   | Out (Mode Int)
   | JT (Mode Int) (Mode Int)     -- jump-if-true
   | JF (Mode Int) (Mode Int)     -- jump-if-false
-  | LT (Mode Int) (Mode Int) Int -- less than
-  | EQ (Mode Int) (Mode Int) Int -- equal
+  | Less (Mode Int) (Mode Int) Int -- less than
+  | Equal (Mode Int) (Mode Int) Int -- equal
   deriving (Show)
 
 data Mode a = Pos a | Imm a
@@ -35,12 +35,17 @@ data Mode a = Pos a | Imm a
 
 readInstruction :: [Int]
                 -> (IntCode,Int) {- ^ (instruction, instruction length) -}
+readInstruction [] = error "cannot decode empty instruction"
 readInstruction (99:_) = ( Halt, 1 )
 readInstruction (n:ns)
   | [1,ma,mb,_] <- decode n = let [a,b,pc] = take 3 ns in ( Sum (mode ma a) (mode mb b) pc, 4 )
   | [2,ma,mb,_] <- decode n = let [a,b,pc] = take 3 ns in ( Mul (mode ma a) (mode mb b) pc, 4 )
-  | (3:_)       <- decode n = let [pa]     = take 1 ns in ( Inp pa, 2 )
+  | [3,_,_,_]   <- decode n = let [pa]     = take 1 ns in ( Inp pa, 2 )
   | [4,ma,_,_]  <- decode n = let [a]      = take 1 ns in ( Out (mode ma a), 2 )
+  | [5,ma,mb,_] <- decode n = let [a,b]    = take 2 ns in ( JT (mode ma a) (mode mb b), 3 )
+  | [6,ma,mb,_] <- decode n = let [a,b]    = take 2 ns in ( JF (mode ma a) (mode mb b), 3 )
+  | [7,ma,mb,_] <- decode n = let [a,b,pc] = take 3 ns in ( Less (mode ma a) (mode mb b) pc, 4 )
+  | [8,ma,mb,_] <- decode n = let [a,b,pc] = take 3 ns in ( Equal (mode ma a) (mode mb b) pc, 4 )
   | otherwise = error $ "cannot decode instruction: " ++ show n
   where
     mode :: Int -> Int -> Mode Int
@@ -60,8 +65,9 @@ run m
   = go m 0 (fetch 0 m)
   where
     go m _ (Halt,_) = m ! 0
-    go m i (op,len) = let m' = step m (traceShowId op)
-                          i' = traceShowId $ i+len
+    go m i (op,len) = let (m',jump) = step m (traceShowId op)
+                          i' | jump /= 0 = jump
+                             | otherwise = i+len
                       in go m' i' (fetch i' m')
 
     fetch :: Int -> Mem -> (IntCode,Int)
@@ -77,18 +83,29 @@ m !~ (Pos n) | Just x <- m !? n = (m,x)
 m !~ (Imm n) = (m,n)
 
 
-step :: Mem -> IntCode -> Mem
-step m (Sum a b pc) = M.insert pc (a' + b') m''
+step :: Mem -> IntCode -> (Mem,Int) {- ^ resulting memory, optional nonzero jump instruction pointer -}
+step m (Sum a b pc) = ( M.insert pc (a' + b') m'', 0 )
   where
     (m' ,a') = m  !~ a
     (m'',b') = m' !~ b
-step m (Mul a b pc) = M.insert pc (a' * b') m''
+step m (Mul a b pc) = ( M.insert pc (a' * b') m'', 0 )
   where
     (m' ,a') = m  !~ a
     (m'',b') = m' !~ b
-step m (Inp pa) = M.insert pa 1 m
-step m (Out ma) = trace ("Out: " ++ (show $ snd $ m !~ ma)) m
-
+step m (Inp pa) = ( M.insert pa 5 {- XXX: thermal radiator controller -} m, 0 )
+step m (Out ma) = ( trace ("Out: " ++ (show $ snd $ m !~ ma)) m, 0 )
+step m (JT a b) = ( m, if 0 /= snd (m !~ a) then snd (m !~ b) else 0 )
+step m (JF a b) = ( m, if 0 == snd (m !~ a) then snd (m !~ b) else 0 )
+step m (Less a b pc) = ( M.insert pc v m'', 0 )
+  where
+    (m' ,a') = m  !~ a
+    (m'',b') = m' !~ b
+    v | a' < b' = 1 | otherwise = 0
+step m (Equal a b pc) = ( M.insert pc v m'', 0 )
+  where
+    (m' ,a') = m  !~ a
+    (m'',b') = m' !~ b
+    v | a' == b' = 1 | otherwise = 0
 step _ Halt        = error "executing Halt"
 
 
