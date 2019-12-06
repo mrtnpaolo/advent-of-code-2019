39db2201325d19297dd56636f91e0fc48a40c811
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-06 10:10:18 +0100

    🎄 day-06: part 2

--- execs/Day06.hs
+++ execs/Day06.hs
@@ -5,31 +5,42 @@ import Data.Tree
 import Data.Graph
 import Control.Arrow ((&&&),(***))
 import Text.Printf
+import Debug.Trace
 
 main =
   do pairs <- map (toPair . words) . lines . map sep <$> getRawInput 6
-     -- print pairs
      let bounds = (minimum *** maximum) $ unzip pairs
-     -- print bounds
      let g = buildG bounds pairs
-     -- print g
-     -- print $ reachable g 4
-     -- putStrLn `mapM_` (test g <$> tests)
-     print $ part1 g 0
+     print g
+     putStrLn $ drawForest $ fmap (fmap show) $ dfs g [0]
+     -- print $ part1 g 0
+     print $ part2 g 1 2
   where
     sep ')' = ' '
     sep x = x
-    -- toPair ["COM",xs] = (0,fromLetter xs)
-    -- toPair [xs,ys] = (fromLetter xs, fromLetter ys)
-    -- fromLetter = (subtract $ fromEnum 'A') . fromEnum . head
     toPair ["COM",ys] = (0,hash3 ys)
+    toPair ["YOU",ys] = (1,hash3 ys)
+    toPair [xs,"YOU"] = (hash3 xs,1)
+    toPair ["SAN",ys] = (2,hash3 ys)
+    toPair [xs,"SAN"] = (hash3 xs,2)
     toPair [xs,ys] = (hash3 xs,hash3 ys)
     hash3 :: String -> Int
+    hash3 [x] = 2 + fromEnum x - fromEnum 'A'
     hash3 [a,b,c] = fromEnum a * 10000 + fromEnum b * 100 + fromEnum c
     hash3 xs = error $ "unexpected: " ++ xs
 
-
 part1 g name = sum . zipWith (\n xs -> n * length xs) [0..] . levels . head . dfs g $ [name]
+
+part2 g from to = abs (depth from - depth v) + abs (depth to - depth v) - 2
+  where
+    lvls :: [[Vertex]]
+    lvls = levels . head . dfs g $ [0]
+    -- v = 5 -- traceShowId $ connection g from to $ head $ dropWhile (not . connected g from to . traceShowId) (reverse lvls)
+    v = head $ head $ dropWhile null [ connections g from to vs | vs <- reverse lvls ]
+
+    depth name = sum $ zipWith (\n lvl -> if name `elem` lvl then n else 0) [0..] lvls
+
+connections g from to vs = [ v | v <- vs, path g v from, path g v to ]
 
 test g (name,expected) =
   printf "node: %2d\texpected: %2d\tgot: %2d\t%s" name expected got passed
