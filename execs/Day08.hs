5720e1789a52b02ab84837b6da3be4612adf4d13
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-08 07:20:50 +0100

    🎄 day-08: part 2

--- execs/Day08.hs
+++ execs/Day08.hs
@@ -11,10 +11,29 @@ main :: IO ()
 main =
   do ns <- map (map digitToInt) . chunksOf (25*6) . init <$> getRawInput 8
      print $ part1 ns
-  where
-    sep x = x
+     print $ length ns
+     -- print `mapM_` part2 ns
+     -- let ts = map (map digitToInt) . chunksOf (2*2) $ "0222112222120000"
+     putStrLn `mapM_` part2 ns
 
--- part1 :: [Int] -> Int
+part1 :: [[Int]] -> Int
 part1 = (\[_,ones,twos] -> ones*twos) .map length . group . sort . minimumBy (comparing zeros)
 
 zeros = length . filter (0==)
+
+part2 :: [[Int]] -> [String]
+part2 = showImage . map squish . transpose . reverse
+
+squish = foldl over 2
+
+over 2 x = x
+over x 2 = x
+over _ 1 = 1
+over _ 0 = 0
+
+showImage = map showLine . chunksOf 25
+
+showLine = map showDigit
+
+showDigit 0 = '#'
+showDigit 1 = ' '
