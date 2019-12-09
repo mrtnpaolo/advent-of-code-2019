2a0a1c946f3e9b60535b10dec08f39df25c70b50
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-09 06:44:05 +0100

    🏋️‍♀️ day-09: refactor
    Didn't actually need `Integer`, 64bits seem enough.

--- common/Advent/IntCode.hs
+++ common/Advent/IntCode.hs
@@ -1,34 +1,34 @@
 {-# LANGUAGE LambdaCase #-}
 module Advent.IntCode where
 
-import Data.List
+import Data.List (unfoldr, genericDrop)
 import Data.Maybe (fromMaybe, listToMaybe)
 
-import Data.Map.Strict (Map, (!?))
-import qualified Data.Map.Strict as M
+import Data.IntMap.Strict (IntMap, (!?))
+import qualified Data.IntMap.Strict as M
 
 import Control.Monad.Trans.RWS.CPS (RWS)
 import qualified Control.Monad.Trans.RWS.CPS as RWS
 
 -- | Running a program
-run :: Mem -> [Integer] {- ^ Inputs -} -> [Integer] {- ^ Outputs -}
+run :: [Int] -> [Int] {- ^ Inputs -} -> [Int] {- ^ Outputs -}
 run = effectuate . eval . load
 
 -- $ Effects
 
 data Effect
-  = Output Integer Effect
-  | Input (Integer -> Effect)
+  = Output Int Effect
+  | Input (Int -> Effect)
   | Stop
 
 -- | Compute the outputs of an effect with the given list of inputs
-effectuate :: Effect -> [Integer] {- ^ Inputs -} -> [Integer] {- ^ Outputs -}
+effectuate :: Effect -> [Int] {- ^ Inputs -} -> [Int] {- ^ Outputs -}
 effectuate Stop         _      = []
 effectuate (Output n e) ins    = n : effectuate e ins
 effectuate (Input _)    []     = error "starved"
 effectuate (Input f)    (x:xs) = effectuate (f x) xs
 
--- | Turn a machine Integero its effect
+-- | Turn a machine Into its effect
 eval :: Machine -> Effect
 eval m =
   case (peek m) of
@@ -41,17 +41,14 @@ eval m =
 -- $ Machine
 
 -- | Machine's memory
-type Mem = Map Integer Integer
-
-fromIntegers :: [Integer] -> Mem
-fromIntegers = M.fromAscList . zip [0..]
+type Mem = IntMap Int
 
 -- | Machine state
-data Machine = Machine { _mem :: Mem, _rb :: Integer, _ip :: Integer, _ins :: [Integer] }
+data Machine = Machine { _mem :: Mem, _rb :: Int, _ip :: Int, _ins :: [Int] }
 
--- | Load memory Integero a machine
-load :: Mem -> Machine
-load mem = Machine { _mem = mem, _rb = 0, _ip = 0, _ins = [] }
+-- | Load memory Into a machine
+load :: [Int] -> Machine
+load mem = Machine { _mem = M.fromAscList (zip [0..] mem), _rb = 0, _ip = 0, _ins = [] }
 
 -- | Peek at the current operation
 peek :: Machine -> IntCode
@@ -63,9 +60,9 @@ peek m = fst . decode . head . genericDrop ip . M.elems . _mem $ m
 
 -- | Parameter modes
 data Mode a
-  = Pos a -- position mode (absolute poIntegerer)
+  = Pos a -- position mode (absolute pointer)
   | Imm a -- immediate mode (value)
-  | Rel a -- relative mode (relative poIntegerer)
+  | Rel a -- relative mode (relative pointer)
 
 naked :: Mode a -> a
 naked (Pos a) = a
@@ -82,7 +79,7 @@ data IntCode
   deriving (Show)
 
 -- | IntCode instruction from opcode
-code :: Integer -> IntCode
+code :: Int -> IntCode
 code  1 = Sum
 code  2 = Mul
 code  3 = Inp
@@ -96,7 +93,7 @@ code 99 = Halt
 code  _ = Unk
 
 -- | IntCode instruction lengths
-len :: IntCode -> Integer
+len :: IntCode -> Int
 len Unk   = 1
 len Sum   = 4
 len Mul   = 4
@@ -109,8 +106,8 @@ len Equal = 4
 len Arb   = 2
 len Halt  = 1
 
--- | Decode an instruction Integero its IntCode and parameter modes
-decode :: Integer -> (IntCode,[Integer])
+-- | Decode an instruction Into its IntCode and parameter modes
+decode :: Int -> (IntCode,[Int])
 decode n = (opcode,modes)
   where
     (m,r) = n `divMod` 100
@@ -122,17 +119,17 @@ decode n = (opcode,modes)
 -- $ IntCode machine
 
 -- | IntCode machine operation
-type OpEnv   = Integer
-type OpLog   = [Integer]
+type OpEnv   = Int
+type OpLog   = [Int]
 type OpState = Machine
 type Op      = RWS OpEnv OpLog OpState
 
 -- | Execute one IntCode operation
-exec1 :: Machine -> Integer -> (Machine,Maybe Integer {- ^ Output -})
+exec1 :: Machine -> Int -> (Machine,Maybe Int {- ^ Output -})
 exec1 m x = listToMaybe <$> RWS.execRWS (fetch >>= uncurry eval1) x m
 
 -- | Fetch the current opcode
-fetch :: Op (IntCode,[Mode Integer]) -- {- ^ (decoded istruction, modal parameters) -}
+fetch :: Op (IntCode,[Mode Int]) -- {- ^ (decoded istruction, modal parameters) -}
 fetch =
   do ip <- RWS.gets _ip
      ~(encoded:params) <- RWS.gets (genericDrop ip . M.elems . _mem)
@@ -140,11 +137,11 @@ fetch =
      pure ( opcode, params @@ modes )
   where
     -- | Decorate parameters with their respective modes
-    (@@) :: [Integer] {- ^ parameters -} -> [Integer] {- ^ modes -} -> [Mode Integer]
+    (@@) :: [Int] {- ^ parameters -} -> [Int] {- ^ modes -} -> [Mode Int]
     ps @@ ms = zipWith (\case 0 -> Pos; 1 -> Imm; 2 -> Rel; _ -> error "unknown mode") ms ps
 
 -- | Evaluate one IntCode operation
-eval1 :: IntCode -> [Mode Integer] {- ^ modal parameters -} -> Op ()
+eval1 :: IntCode -> [Mode Int] {- ^ modal parameters -} -> Op ()
 
 eval1 i@Inp   (a:_)     = do set a =<< ask
                              next i
@@ -171,10 +168,10 @@ eval1 i@Sum   (a:b:c:_) = do set c =<< (+) <$> get a <*> get b
 eval1 i@Mul   (a:b:c:_) = do set c =<< (*) <$> get a <*> get b
                              next i
 
-eval1 i@Less  (a:b:c:_) = do set c =<< fmap (fromIntegral . fromEnum) ((<) <$> get a <*> get b)
+eval1 i@Less  (a:b:c:_) = do set c =<< fmap fromEnum ((<) <$> get a <*> get b)
                              next i
 
-eval1 i@Equal (a:b:c:_) = do set c =<< fmap (fromIntegral . fromEnum) ((==) <$> get a <*> get b)
+eval1 i@Equal (a:b:c:_) = do set c =<< fmap fromEnum ((==) <$> get a <*> get b)
                              next i
 
 eval1   Halt  _         = error "eval1: Halt"
@@ -182,34 +179,34 @@ eval1   Halt  _         = error "eval1: Halt"
 eval1   Unk   _         = error "eval1: Unk"
 
 -- | Read a modal parameter
-get :: Mode Integer -> Op Integer
+get :: Mode Int -> Op Int
 get (Imm x)  = pure x
 get (Pos px) = RWS.gets (fromMaybe 0 . (!? px) . _mem)
 get (Rel rx) = RWS.gets _rb >>= \rb ->
                  RWS.gets (fromMaybe 0 . (!? (rx+rb)) . _mem)
 
 -- | Write to a modal parameter
-set :: Mode Integer -> Integer -> Op ()
+set :: Mode Int -> Int -> Op ()
 set (Imm _) _ = error "setting to an immediate"
 set (Pos x) n = RWS.modify (\m -> m { _mem = M.insert x n (_mem m) })
 set (Rel x) n = RWS.gets _rb >>= \rb ->
                   RWS.modify (\m -> m { _mem = M.insert (x + rb) n (_mem m) })
 
-adjustRelativeBase :: Integer -> Op ()
+adjustRelativeBase :: Int -> Op ()
 adjustRelativeBase n = RWS.modify $ \m -> m { _rb = _rb m + n }
 
 -- | Read the input
-ask :: Op Integer
+ask :: Op Int
 ask = RWS.ask
 
--- | Increment the instruction poIntegerer
-incrIp :: Integer -> Op ()
+-- | Increment the instruction pointer
+incrIp :: Int -> Op ()
 incrIp n = RWS.gets _ip >>= setIp . (n +)
 
--- | Set the instruction poIntegerer
-setIp :: Integer -> Op ()
+-- | Set the instruction pointer
+setIp :: Int -> Op ()
 setIp n = RWS.modify (\m -> m { _ip = n })
 
--- | Advance the instruction poIntegerer to the next instruction
+-- | Advance the instruction pointer to the next instruction
 next :: IntCode -> Op ()
 next = incrIp . len
--- execs/Day09.hs
+++ execs/Day09.hs
@@ -6,12 +6,19 @@ import Advent.IntCode
 
 main :: IO ()
 main =
-  do raw :: [Integer] <- map read . words . map sep <$> getRawInput 9
-     print $ quine [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
-     print $ run (fromIntegers raw) [1]
-     print $ run (fromIntegers raw) [2]
+  do mem <- map read . words . map sep <$> getRawInput 9
+     -- runTests
+     print (run mem [1])
+     print (run mem [2])
   where
     sep ',' = ' '
     sep x = x
 
-    quine mem = run (fromIntegers mem) [] == mem
+runTests :: IO ()
+runTests = mapM_ print [
+    quine [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99],
+    16 == (length . show . head $ run [1102,34915192,34915192,7,4,7,99,0] []),
+    1125899906842624 == (head $ run [104,1125899906842624,99] [])
+  ]
+  where
+    quine mem = run mem [] == mem
