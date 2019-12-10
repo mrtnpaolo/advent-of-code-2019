d2bb5aceb8a855950c6e1bd054462a2aeefce2ee
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-10 11:11:13 +0100

    🛠 day-10: wip
    😓

--- execs/Day10.hs
+++ execs/Day10.hs
@@ -8,11 +8,16 @@ import qualified Data.Set as S
 import Data.Map.Strict (Map, (!?))
 import qualified Data.Map.Strict as M
 
+import Data.Bool (bool)
 import Data.Char (intToDigit)
+import Data.List (nub, (\\))
 import Data.Maybe (fromMaybe)
-import Data.Traversable (forM)
+import Data.Bifunctor (second)
+import Data.Traversable (for)
+import Data.Foldable (forM_)
 
 import Text.Printf
+import Debug.Trace
 
 main :: IO ()
 main =
@@ -21,62 +26,90 @@ main =
      let (xM,yM) = (maximum (S.map fst space), maximum (S.map snd space))
      print xM
      print yM
-     -- mapM_ print (centeredRays (2,2) (xM,yM))
-     let s' = part1 space
-     mapM_ print $ S.elems $ s'
-     putStr . showVis (xM,yM) . M.fromList . S.elems $ s'
+
+     let test02 = rays space (0,2)
+     putStrLn "=== rays from (0,2)"
+     forM_ test02 $ \cs ->
+       putStrLn $ showRay (size space) (0,2) (S.fromList cs)
+
+     -- putStrLn "=== rays from (2,2)"
+     -- mapM_ print (rays space (2,2))
+     putStrLn "=== part 1"
+     mapM_ print (part1 space)
+     putStrLn $ showVis (size space) (part1 space)
 
 type Coord = (Int,Int)
 type Space = Set Coord
-type Visibility = Map Coord Int
 
-part1 space = S.map visibleFrom space
+size space = (maximum (S.map fst space), maximum (S.map snd space))
+
+part1 space = illuminate space
   where
-    (xM,yM) = (maximum (S.map fst space), maximum (S.map snd space))
+    illuminate = S.map (\p -> (p,count $ rays space p))
+    count = length . filter (any (`S.member` space))
 
-    visibleFrom (cx,cy) = (\xs -> ((cx,cy),length xs)) . filter (any asteroid) $ centeredRays (cx,cy) (xM,yM)
-
-    asteroid = (`S.member` space)
-
-showVis :: (Int,Int) -> Visibility -> String
-showVis (xM,yM) vs = unlines $
+showRay (xM,yM) center ray = unlines $
   (flip map) [0..yM] $ \y ->
     concat $ (flip map) [0..xM] $ \x ->
-      -- fromMaybe '.' (intToDigit <$> vs !? (x,y))
+      if (x,y) == center
+        then "  O"
+        else bool "  ." "  R" ((x,y) `S.member` ray)
+
+showVis (xM,yM) v = unlines $
+  (flip map) [0..yM] $ \y ->
+    concat $ (flip map) [0..xM] $ \x ->
       fromMaybe "  ." (printf " %2d" <$> vs !? (x,y))
+  where
+    vs = M.fromList (S.elems v)
 
 parse :: String -> Space
 parse raw = S.fromList [ (x,y) | (y,ys) <- space, (x,v) <- ys, v == '#' ]
   where
     space = zip [0..] (map (zip [0..]) (lines raw))
 
--- | Finite rays in all directions clockwise from the positive X axis around (cx,cy)
-centeredRays :: (Int,Int) -> (Int,Int) -> [[Coord]]
-centeredRays (cx,cy) (xM,yM) = filter (not . null) . map (takeWhile inside . map (translate (cx,cy))) $ rays (xM,yM)
+rays :: Space -> (Int,Int) -> [[Coord]]
+rays space (cx,cy) = traceShowId $ map ray sweep
   where
-    translate (cx,cy) (x,y) = (cx+x,cy+y)
+    (xM,yM) = size space
+
+    translate (x,y) (dx,dy) = (x+dx,y+dy)
+
     inside (x,y) = 0 <= x && x <= xM && 0 <= y && y <= yM
 
--- | Lazy infinite rays in all directions clockwise from the positive X axis around the origin
-rays :: (Int {- ^ max x -}, Int {- ^ max y -}) -> [[Coord]]
-rays (xM,yM)
-  | xM < 2 || yM < 2 = error $ "can't cast rays in a " ++ show xM ++ "x" ++ show yM ++ " space"
-  | otherwise      = concat [ [x14], q4, [y34], q3, [x23], q2, [y12], q1 ]
-  where
-    -- ray in the second quadrant
-    ray (x,y) = takeWhile (\(x,y) -> x<xM&&y<yM) $ scanl (\(a,b)(c,d)->(a+c,b+d)) (x,y) (repeat (x,y))
-    q4 = [ ray (x,y) | y <- [1,2..yM], x <- [xM,xM-1..1] ]
-    q3 = map (map (\(x,y) -> (-x,y))) q4
-    q2 = map (map (\(x,y) -> (x,-y))) q3
-    q1 = map (map (\(x,y) -> (x,-y))) q2
-    x14 = [ ( x,  0) | x <- [1..] ]
-    y34 = [ ( 0,  y) | y <- [1..] ]
-    x23 = [ (-x,  0) | x <- [1..] ]
-    y12 = [ ( 0, -y) | y <- [1..] ]
+    ray (dx,dy) = takeWhile inside $ tail $ iterate (\(x,y)->(x+dx,y+dy)) (cx,cy)
 
-    -- filterRecasts = go S.empty
-    --   where
-    --     go _ [] = []
-    --     go seen (r:rs)
-    --       | any (`S.member` seen) r =     go seen                            rs
-    --       | otherwise               = r : go (seen `S.union` (S.fromList r)) rs
+    --      x               xM
+    --   y  . . . \ . / . . .
+    --      . . . \ . / . . .
+    --      \ \ \ \ | / / / /
+    --      . . . - C - . . .
+    --      / / / / | \ \ \ \
+    --      . . . / . \ . . .
+    --   yM . . . / . \ . . .
+    --
+    --        x 1 2 3 4 5 6 7 xM=8
+    --                F   G
+    --   y    . . . .-4 .-4 . .
+    --   1    . . . .-3 .-3 . .
+    --   2    . . . .-2 .-2 . .
+    --   3 E -5-4-3-2 \ | / 2 3 H
+    --   4    . . . . - C - . .      C = (5,4)
+    --   5 D -5-4-3-2 / | \ 2 3 A
+    --   6    . . . . 2 . 2 . .
+    --   7    . . . . 3 . 3 . .
+    -- 8=yM   . . . . 4 . 4 . .
+    --                C   B
+    sweep = axes ++ diags ++ crooked
+      where
+        axes = [ (1,0), (0,1), (-1,0), (0,-1) ]
+        diags = [ (1,1), (-1,1), (-1,-1), (1,-1) ]
+        crooked = concat $ takeWhile (inside . translate (cx,cy)) <$>
+          [ [ ( dx,  1) | dx <- [2,3..] ] -- A
+          , [ (  1, dy) | dy <- [2,3..] ] -- B
+          , [ ( -1, dy) | dy <- [2,3..] ] -- C
+          , [ (-dx,  1) | dx <- [2,3..] ] -- D
+          , [ (-dx, -1) | dx <- [2,3..] ] -- E
+          , [ ( -1,-dy) | dy <- [2,3..] ] -- F
+          , [ (  1,-dy) | dy <- [2,3..] ] -- G
+          , [ ( dx, -1) | dx <- [2,3..] ] -- H
+          ]
