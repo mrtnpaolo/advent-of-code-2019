8d2de224f1b573e8d09684d63d55a26b9deb4169
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-10 13:12:56 +0100

    🎄 day-10: part 1
    Hadn't occurred to me to think about the GCD until very late!

--- execs/Day10.hs
+++ execs/Day10.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 module Main (main) where
 
 import Advent
@@ -9,39 +10,28 @@ import Data.Map.Strict (Map, (!?))
 import qualified Data.Map.Strict as M
 
 import Data.Ord (comparing)
-import Data.Bool (bool)
-import Data.Char (intToDigit)
-import Data.List (nub, (\\), maximumBy)
+import Data.List (maximumBy)
 import Data.Maybe (fromMaybe)
 import Data.Bifunctor
-import Data.Traversable (for)
-import Data.Foldable (forM_)
 
 import Text.Printf
-import Debug.Trace
 
 main :: IO ()
 main =
   do space <- parse <$> getRawInput 10
-     print space
-     let (xM,yM) = (maximum (S.map fst space), maximum (S.map snd space))
-     print xM
-     print yM
 
-     putStrLn "=== part 1"
      let detections = part1 space
-     putStrLn $ showVis (size space) detections
+     putStrLn $ showVis space detections
      print $ maximumBy (comparing snd) $ M.assocs detections
 
 type Coord = (Int,Int)
-type Space = Set Coord
+data Space = Space { space_ :: Set Coord, w_ :: Int, h_ :: Int }
+  deriving Show
 
 size space = (maximum (S.map fst space), maximum (S.map snd space))
 
-part1 :: Set Coord -> Map Coord Int
-part1 space = x
-  where
-    x = M.fromList [ (p,detect p space) | p <- S.elems space ]
+part1 :: Space -> Map Coord Int
+part1 Space{..} = M.fromList [ (p,detect p space_) | p <- S.elems space_ ]
 
 detect :: Coord -> Set Coord -> Int
 detect p space = sweep 0 (S.delete p space) (tail $ map (translate p) spiral)
@@ -54,10 +44,7 @@ detect p space = sweep 0 (S.delete p space) (tail $ map (translate p) spiral)
       -- asteroid is detected
       | q `S.member` space =
         let rs = S.fromList $ takeWhile inside (ray p q)
-            dbg = if (22,26) == p
-                    then trace (printf "target: (%d,%d) %s" (fst p) (snd p) (show rs) :: String) 
-                    else id
-        in dbg $ sweep (succ n) (space `S.difference` rs) qs
+        in sweep (succ n) (space `S.difference` rs) qs
 
       | otherwise          = sweep n space qs
 
@@ -71,13 +58,13 @@ ray :: Coord -> Coord -> [Coord]
 ray (x,y) (x',y') = tail $ iterate (\(x,y)->(x+dx,y+dy)) (x,y)
   where
     (dx,dy)
-      -- 45 deg
-      | abs (x'-x) == abs (y'-y) = (signum $ x'-x,signum $ y'-y) 
+      -- diagonals
+      | abs (x'-x) == abs (y'-y) = (signum $ x'-x,signum $ y'-y)
       -- horizontal & vertical
       | x' == x = (0,signum $ y'-y)
       | y' == y = (signum $ x'-x,0)
       -- crooked
-      | otherwise = (x'-x,y'-y)
+      | otherwise = ((x'-x) `div` k,(y'-y) `div` k) where k = gcd (x'-x) (y'-y)
 
 -- counter-clockwise spiral
 spiral :: [Coord]
@@ -89,19 +76,24 @@ spiral = scanl go (0,0) $ concat [ replicate ((i `div` 2) + 1) $ off (i `mod` 4)
     off 3 = bimap id succ
     go (x,y) f = f (x,y)
 
-showRay (xM,yM) center ray = unlines $
-  (flip map) [0..yM] $ \y ->
-    concat $ (flip map) [0..xM] $ \x ->
-      if (x,y) == center
-        then "  O"
-        else bool "  ." "  R" ((x,y) `S.member` ray)
+-- showRay (xM,yM) center ray = unlines $
+--   (flip map) [0..yM] $ \y ->
+--     concat $ (flip map) [0..xM] $ \x ->
+--       if (x,y) == center
+--         then "  O"
+--         else bool "  ." "  R" ((x,y) `S.member` ray)
 
-showVis (xM,yM) counts = unlines $
-  (flip map) [0..yM] $ \y ->
-    concat $ (flip map) [0..xM] $ \x ->
+showVis :: Space -> Map Coord Int -> String
+showVis space@Space{..} counts = unlines $
+  (flip map) [0..h_-1] $ \y ->
+    concat $ (flip map) [0..w_-1] $ \x ->
       fromMaybe "   ." (printf " %3d" <$> counts !? (x,y))
 
 parse :: String -> Space
-parse raw = S.fromList [ (x,y) | (y,ys) <- space, (x,v) <- ys, v == '#' ]
+parse raw = Space
+  { space_ = S.fromList [ (x,y) | (y,ys) <- space, (x,v) <- ys, v == '#' ]
+  , h_ = length space
+  , w_ = length (snd $ head space)
+  }
   where
     space = zip [0..] (map (zip [0..]) (lines raw))
