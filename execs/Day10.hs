9259a6c8f995e70e1f4b4174d1e7dca424389dff
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-10 12:31:06 +0100

    🛠 day-10: wip, different approach, passes all the tests, not the input
    😞

--- execs/Day10.hs
+++ execs/Day10.hs
@@ -8,11 +8,12 @@ import qualified Data.Set as S
 import Data.Map.Strict (Map, (!?))
 import qualified Data.Map.Strict as M
 
+import Data.Ord (comparing)
 import Data.Bool (bool)
 import Data.Char (intToDigit)
-import Data.List (nub, (\\))
+import Data.List (nub, (\\), maximumBy)
 import Data.Maybe (fromMaybe)
-import Data.Bifunctor (second)
+import Data.Bifunctor
 import Data.Traversable (for)
 import Data.Foldable (forM_)
 
@@ -27,26 +28,66 @@ main =
      print xM
      print yM
 
-     let test02 = rays space (0,2)
-     putStrLn "=== rays from (0,2)"
-     forM_ test02 $ \cs ->
-       putStrLn $ showRay (size space) (0,2) (S.fromList cs)
-
-     -- putStrLn "=== rays from (2,2)"
-     -- mapM_ print (rays space (2,2))
      putStrLn "=== part 1"
-     mapM_ print (part1 space)
-     putStrLn $ showVis (size space) (part1 space)
+     let detections = part1 space
+     putStrLn $ showVis (size space) detections
+     print $ maximumBy (comparing snd) $ M.assocs detections
 
 type Coord = (Int,Int)
 type Space = Set Coord
 
 size space = (maximum (S.map fst space), maximum (S.map snd space))
 
-part1 space = illuminate space
+part1 :: Set Coord -> Map Coord Int
+part1 space = x
   where
-    illuminate = S.map (\p -> (p,count $ rays space p))
-    count = length . filter (any (`S.member` space))
+    x = M.fromList [ (p,detect p space) | p <- S.elems space ]
+
+detect :: Coord -> Set Coord -> Int
+detect p space = sweep 0 (S.delete p space) (tail $ map (translate p) spiral)
+  where
+    sweep :: Int -> Set Coord -> [Coord] -> Int
+    sweep n space (q:qs)
+
+      | S.null space       = n
+
+      -- asteroid is detected
+      | q `S.member` space =
+        let rs = S.fromList $ takeWhile inside (ray p q)
+            dbg = if (22,26) == p
+                    then trace (printf "target: (%d,%d) %s" (fst p) (snd p) (show rs) :: String) 
+                    else id
+        in dbg $ sweep (succ n) (space `S.difference` rs) qs
+
+      | otherwise          = sweep n space qs
+
+    inside (x,y) = 0 <= x && x <= xM && 0 <= y && y <= yM
+
+    (xM,yM) = size space
+
+translate (x,y) (dx,dy) = (x+dx,y+dy)
+
+ray :: Coord -> Coord -> [Coord]
+ray (x,y) (x',y') = tail $ iterate (\(x,y)->(x+dx,y+dy)) (x,y)
+  where
+    (dx,dy)
+      -- 45 deg
+      | abs (x'-x) == abs (y'-y) = (signum $ x'-x,signum $ y'-y) 
+      -- horizontal & vertical
+      | x' == x = (0,signum $ y'-y)
+      | y' == y = (signum $ x'-x,0)
+      -- crooked
+      | otherwise = (x'-x,y'-y)
+
+-- counter-clockwise spiral
+spiral :: [Coord]
+spiral = scanl go (0,0) $ concat [ replicate ((i `div` 2) + 1) $ off (i `mod` 4) | i <- [0..] ]
+  where
+    off 0 = bimap succ id
+    off 1 = bimap id pred
+    off 2 = bimap pred id
+    off 3 = bimap id succ
+    go (x,y) f = f (x,y)
 
 showRay (xM,yM) center ray = unlines $
   (flip map) [0..yM] $ \y ->
@@ -55,61 +96,12 @@ showRay (xM,yM) center ray = unlines $
         then "  O"
         else bool "  ." "  R" ((x,y) `S.member` ray)
 
-showVis (xM,yM) v = unlines $
+showVis (xM,yM) counts = unlines $
   (flip map) [0..yM] $ \y ->
     concat $ (flip map) [0..xM] $ \x ->
-      fromMaybe "  ." (printf " %2d" <$> vs !? (x,y))
-  where
-    vs = M.fromList (S.elems v)
+      fromMaybe "   ." (printf " %3d" <$> counts !? (x,y))
 
 parse :: String -> Space
 parse raw = S.fromList [ (x,y) | (y,ys) <- space, (x,v) <- ys, v == '#' ]
   where
     space = zip [0..] (map (zip [0..]) (lines raw))
-
-rays :: Space -> (Int,Int) -> [[Coord]]
-rays space (cx,cy) = traceShowId $ map ray sweep
-  where
-    (xM,yM) = size space
-
-    translate (x,y) (dx,dy) = (x+dx,y+dy)
-
-    inside (x,y) = 0 <= x && x <= xM && 0 <= y && y <= yM
-
-    ray (dx,dy) = takeWhile inside $ tail $ iterate (\(x,y)->(x+dx,y+dy)) (cx,cy)
-
-    --      x               xM
-    --   y  . . . \ . / . . .
-    --      . . . \ . / . . .
-    --      \ \ \ \ | / / / /
-    --      . . . - C - . . .
-    --      / / / / | \ \ \ \
-    --      . . . / . \ . . .
-    --   yM . . . / . \ . . .
-    --
-    --        x 1 2 3 4 5 6 7 xM=8
-    --                F   G
-    --   y    . . . .-4 .-4 . .
-    --   1    . . . .-3 .-3 . .
-    --   2    . . . .-2 .-2 . .
-    --   3 E -5-4-3-2 \ | / 2 3 H
-    --   4    . . . . - C - . .      C = (5,4)
-    --   5 D -5-4-3-2 / | \ 2 3 A
-    --   6    . . . . 2 . 2 . .
-    --   7    . . . . 3 . 3 . .
-    -- 8=yM   . . . . 4 . 4 . .
-    --                C   B
-    sweep = axes ++ diags ++ crooked
-      where
-        axes = [ (1,0), (0,1), (-1,0), (0,-1) ]
-        diags = [ (1,1), (-1,1), (-1,-1), (1,-1) ]
-        crooked = concat $ takeWhile (inside . translate (cx,cy)) <$>
-          [ [ ( dx,  1) | dx <- [2,3..] ] -- A
-          , [ (  1, dy) | dy <- [2,3..] ] -- B
-          , [ ( -1, dy) | dy <- [2,3..] ] -- C
-          , [ (-dx,  1) | dx <- [2,3..] ] -- D
-          , [ (-dx, -1) | dx <- [2,3..] ] -- E
-          , [ ( -1,-dy) | dy <- [2,3..] ] -- F
-          , [ (  1,-dy) | dy <- [2,3..] ] -- G
-          , [ ( dx, -1) | dx <- [2,3..] ] -- H
-          ]
