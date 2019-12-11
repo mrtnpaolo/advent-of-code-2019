9d33355fb219ebddc41d88411a387483d2da997e
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-11 11:13:40 +0100

    🎄 day-10: part 2
    What a headache! My relative coordinate transform wasn't correct.
    In particular `sub` had its arguments reversed!

--- execs/Day10.hs
+++ execs/Day10.hs
@@ -4,6 +4,7 @@ import Advent
 
 import Data.Ord
 import Data.Bool
+import Data.List
 import Data.Foldable
 
 import Data.Set (Set)
@@ -15,15 +16,39 @@ main :: IO ()
 main =
   do asteroids <- S.fromList . parse <$> getRawInput 10
      printSpace asteroids
-     print (part1 asteroids)
+     let (base,count) = part1 asteroids
+     print base
+     print count
+     -- print (((part2 base asteroids) !!) <$> map pred [1,2,3,10,20,50,100,199,200,201,299])
+     print . (\(x,y) -> x*100 + y) $ part2 base asteroids !! pred 200
   where
     parse raw = [ (x,y) | (y,ys) <- zip [0..] (lines raw), (x,'#') <- zip [0..] ys ]
 
 type Coord = (Int,Int)
 type Space = Set Coord
 
-part1 :: Space -> Int
-part1 coords = snd $ maximumBy (comparing snd) [ (p,visible coords p) | p <- toList coords ]
+part1 :: Space -> (Coord,Int)
+part1 coords = maximumBy (comparing snd) [ (p,visible coords p) | p <- toList coords ]
+
+part2 :: Coord -> Space -> [Coord]
+part2 center coords = map (add center) $ go relative
+  where
+    relative = S.map (sub center) (S.delete center coords)
+
+    -- go :: Space -> [(Coord,Double)]
+    go asteroids
+      | S.null asteroids = []
+      | otherwise        = vaporized ++ go asteroids'
+      where
+        asteroids' = S.difference asteroids (S.fromList vaporized)
+        vaporized = sortOn angle [ p | p <- toList asteroids, visibleFromOrigin asteroids p ]
+        angle :: Coord -> Double
+        angle (x,y) = rotate $ atan2 (fromIntegral y) (fromIntegral x)
+          where
+            rotate :: Double -> Double
+            rotate a = let b = a + pi/2
+                           k = fromIntegral (floor $ b / (2*pi))
+                       in b - 2*pi * k
 
 visible :: Space -> Coord -> Int
 visible coords center = count (visibleFromOrigin relative) (S.delete (0,0) relative)
@@ -42,8 +67,11 @@ visibleFromOrigin coords (x,y) = no obstacle between
     obstacle = flip S.member coords
     between = [ (i * u,i * v) | i <- [1..k-1] ]
 
+add :: Coord -> Coord -> Coord
+add (x,y) (u,v) = (x+u,y+v)
+
 sub :: Coord -> Coord -> Coord
-sub (x,y) (u,v) = (x-u,y-v)
+sub (x,y) (u,v) = (u-x,v-y)
 
 printSpace :: Space -> IO ()
 printSpace coords =
