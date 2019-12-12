401799c422aea918d094b8dcfe0efddb9c5b7f09
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-12 23:31:40 +0100

    🛠 day-12: generalize dimensions and number of moons
    This lost speed again, back to ~2s, it can be improved.
    
    It also includes the speedup of looking only at velocities, whose
    periods comprise half of the whole system's ones.

--- execs/Day12.hs
+++ execs/Day12.hs
@@ -1,120 +1,69 @@
 {-# LANGUAGE BangPatterns #-}
 module Main (main) where
 
-import Data.Bifunctor
+import Data.List
 
 main :: IO ()
 main =
-  do print (energy (after 1000 moons))
-     print t
+  do print (energy (after 10 moons))
+     print (part2 moons)
   where
     after n = head . drop n . iterate step
 
-    (tx,ty,tz) = part2 moons
-    t = lcm (lcm tx ty) tz
-
-type P = (Int,Int,Int) -- position
-type V = (Int,Int,Int) -- velocity
+type P = [Int] -- position
+type V = [Int] -- velocity
 type Moon = (P,V)
-type Moons = (Moon,Moon,Moon,Moon)
+type Moons = [Moon]
 
 moons :: Moons
 moons =
-  ( (( 4,  1, 1),(0,0,0))
-  , ((11,-18,-1),(0,0,0))
-  , ((-2,-10,-4),(0,0,0))
-  , ((-7, -2,14),(0,0,0)) )
+  [ ( [ 4,  1, 1] , [0,0,0] )
+  , ( [11,-18,-1] , [0,0,0] )
+  , ( [-2,-10,-4] , [0,0,0] )
+  , ( [-7, -2,14] , [0,0,0] ) ]
 
 -- moons :: Moons
 -- moons =
---   ( ((-1, 0,  2),(0,0,0))
---   , (( 2,-10,-7),(0,0,0))
---   , (( 4, -8, 8),(0,0,0))
---   , (( 3,  5,-1),(0,0,0)) )
+--   [ ( [-1, 0,  2] , [0,0,0] )
+--   , ( [ 2,-10,-7] , [0,0,0] )
+--   , ( [ 4, -8, 8] , [0,0,0] )
+--   , ( [ 3,  5,-1] , [0,0,0] ) ]
 
-opp :: (Int,Int,Int) -> (Int,Int,Int)
-opp (!a,!b,!c) = (-a,-b,-c)
-
-(<+>) :: (Int,Int,Int) -> (Int,Int,Int) -> (Int,Int,Int)
-(!a,!b,!c) <+> (d,!e,!f) = (a+d,b+e,c+f)
+(<+>) :: [Int] -> [Int] -> [Int]
+u <+> v = zipWith (+) u v
 {-# INLINE (<+>) #-}
 
-gravity1 :: Int -> Int -> Int
-gravity1 !a !b = case compare a b of LT -> -1; EQ -> 0; GT -> 1
-
-gravity :: Moon -> Moon -> V
-gravity ((!x0,!y0,!z0),_) ((!x1,!y1,!z1),_) =
-  ( gravity1 x0 x1
-  , gravity1 y0 y1
-  , gravity1 z0 z1 )
-
-effects :: Moons -> (V,V,V,V)
-effects (!m0,!m1,!m2,!m3) =
-  (     g01 <+>     g02 <+>     g03
-  , opp g01 <+>     g12 <+>     g13
-  , opp g02 <+> opp g12 <+>     g23
-  , opp g03 <+> opp g13 <+> opp g23
-  )
+gravity :: P -> P -> V
+gravity !p !q = zipWith comp p q
   where
-    g01 = gravity m1 m0
-    g02 = gravity m2 m0
-    g03 = gravity m3 m0
-    g12 = gravity m2 m1
-    g13 = gravity m3 m1
-    g23 = gravity m3 m2
-{-# INLINE effects #-}
+    comp !a !b = case compare a b of LT -> -1; EQ -> 0; GT -> 1
+{-# INLINE gravity #-}
 
 applyGravity :: Moons -> Moons
-applyGravity ms@(!m0,!m1,!m2,!m3) = (second (v0 <+>) m0,second (v1 <+>) m1,second (v2 <+>) m2,second (v3 <+>) m3)
-  where
-    (!v0,!v1,!v2,!v3) = effects ms
-{-# INLINE applyGravity #-}
+applyGravity ms = [ (x,foldl' (<+>) v [ gravity y x | (y,_) <- ms ]) | (x,v) <- ms ]
 
 applyVelocity :: Moons -> Moons
-applyVelocity (!m0,!m1,!m2,!m3) = (f m0,f m1,f m2,f m3)
-  where
-    f ((!x,!y,!z),v@(!vx,!vy,!vz)) = ((x+vx,y+vy,z+vz),v)
-{-# INLINE applyVelocity #-}
+applyVelocity ms = [ (x <+> v,v) | (x,v) <- ms ]
 
 step :: Moons -> Moons
 step = applyVelocity . applyGravity
-{-# INLINE step #-}
 
 energy :: Moons -> Int
-energy (!m0,!m1,!m2,!m3) = e m0 + e m1 + e m2 + e m3
+energy = sum . map e
   where
     e m = p m * k m
 
-    p ((!x,!y,!z),_) = abs x + abs y + abs z
-    k (_,(!vx,!vy,!vz)) = abs vx + abs vy + abs vz
+    p (x,_) = sum (map abs x)
+    k (_,v) = sum (map abs v)
 
-type Proj = (Int,Int,Int,Int,Int,Int,Int,Int)
-
-project :: Moons -> (Proj,Proj,Proj)
-project ms = (x,y,z)
+part2 :: Moons -> Int
+part2 ms = (2*) . lcm' . map (succ . findRepeat) . byDimension . velocities . iterate step $ ms
   where
-    (  ((!x0,!y0,!z0),(!vx0,!vy0,!vz0))
-     , ((!x1,!y1,!z1),(!vx1,!vy1,!vz1))
-     , ((!x2,!y2,!z2),(!vx2,!vy2,!vz2))
-     , ((!x3,!y3,!z3),(!vx3,!vy3,!vz3)) ) = ms
+    velocities = map (map snd)
+    byDimension = take dims . transpose . map transpose
+    dims = length.fst.head $ ms
+    lcm' = foldl' lcm 1
 
-    x = (x0,vx0,x1,vx1,x2,vx2,x3,vx3)
-    y = (y0,vy0,y1,vy1,y2,vy2,y3,vy3)
-    z = (z0,vz0,z1,vz1,z2,vz2,z3,vz3)
-{-# INLINE project #-}
-
-part2 :: Moons -> (Int,Int,Int) {- ^ Periods of repetition for each axis -}
-part2 ms = go 1 0 0 0 (tail $ iterate step ms)
-  where
-    (startx,starty,startz) = project ms
-
-    -- compute minimum times on each axis independently
-    go :: Int -> Int -> Int -> Int -> [Moons] -> (Int,Int,Int)
-    go !i !tx !ty !tz (!ms:mss)
-      | tx < 1 || ty < 1 || tz < 1 = go (succ i) tx' ty' tz' mss
-      | otherwise                  = (tx,ty,tz)
-      where
-        (x,y,z) = project ms
-        tx' = if tx < 1 && startx == x then i else tx
-        ty' = if ty < 1 && starty == y then i else ty
-        tz' = if tz < 1 && startz == z then i else tz
+findRepeat :: Eq a => [a] -> Int
+findRepeat [] = error "empty list"
+findRepeat (x:xs) = let Just i = elemIndex x xs in i
