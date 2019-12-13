02b0a9348b6f53da52f896144b3617ea5fb4ef9b
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-13 06:06:36 +0100

    🎄 day-13: part 1

--- execs/Day13.hs
+++ execs/Day13.hs
@@ -1,8 +1,20 @@
 module Main (main) where
 
 import Advent
-
+import Advent.IntCode
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
 main :: IO ()
 main =
-  do raw <- getRawInput 13
-     print raw
+  do mem <- map (read::String->Int) . words . map sep <$> getRawInput 13
+     print (part1 mem)
+  where sep ',' = ' '; sep x = x
+
+part1 mem = M.size $ go M.empty (run' mem)
+  where
+    go m (Output x (Output y (Output tile eff))) =
+      case tile of
+        2 -> go (M.insertWith (+) (x,y) 1 m) eff
+        _ -> go m eff
+    go m (Input f) = error "dunno"
+    go m Stop = m
