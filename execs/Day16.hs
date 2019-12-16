c69c422ed329f8e3fd3d1392b8867e8388dd651f
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-16 06:29:52 +0100

    🎄 day-16: part 1

--- execs/Day16.hs
+++ execs/Day16.hs
@@ -1,8 +1,24 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Main (main) where
 
 import Advent
 
+import Data.Char
+
 main :: IO ()
 main =
-  do raw <- getRawInput 16
-     print raw
+  do xs <- map digitToInt . init <$> getRawInput 16
+     let ys = take 8 $ head $ drop 100 (iterate fft xs)
+     print ys
+
+base :: [Int]
+base = [0,1,0,-1]
+
+pattern :: Int -> [Int]
+pattern n = cycle (base >>= replicate n)
+
+fft :: [Int] -> [Int]
+fft xs = zipWith dot (repeat xs) $ take (length xs) (map (take (length xs) . tail . pattern) [1..])
+
+dot :: [Int] -> [Int] -> Int
+dot xs ys = (\n -> abs n `mod` 10) (sum (zipWith (*) xs ys))
