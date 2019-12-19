4392047c6966ff7510e7035e91326b5fa096dc1a
Paolo Martini <mrtnpaolo@protonmail.com>
2019-12-19 10:44:44 +0100

    🛠 day-18: checkpoint
    My flood fill was bugged in 2 ways:
    
      - `found` too big because wasn't checking against only missing keys
      - `candidates` too big because of non-pruned repeated `adj` results

--- execs/Day18.hs
+++ execs/Day18.hs
@@ -14,112 +14,46 @@ import Debug.Trace
 
 main :: IO ()
 main =
-  do m1 <- parse <$> readFile "inputs/input18-test01"
-     let g1 = build m1
-     prettyPrint g1
-     let ends1 = filter (M.null . fromKeys . snd . fromJust . lab g1) (nodes g1)
-     putStrLn ("g1 has " ++ show (length ends1) ++ " ends: " ++ show ends1)
-     mapM_ print [ spLength 0 n g1 | n <- ends1 ]
+  do m <- parse <$> getRawInput 18 -- readFile "inputs/input18-test02" -- getRawInput 18
+     let sol = findOneSolution m (entrance m,keys m)
+     print `mapM_` sol
+     print (sum (map snd sol))
 
-     m2 <- parse <$> readFile "inputs/input18-test02"
-     let g2 = build m2
-     prettyPrint g2
-     let ends2 = filter (M.null . fromKeys . snd . fromJust . lab g2) (nodes g2)
-     putStrLn ("g2 has " ++ show (length ends2) ++ " ends: " ++ show ends2)
-     mapM_ print [ spLength 0 n g2 | n <- ends2 ]
-
-     m3 <- parse <$> getRawInput 18
-     let g3 = build m3
-     prettyPrint g3
-     let ends3 = filter (M.null . fromKeys . snd . fromJust . lab g3) (nodes g3)
-     putStrLn ("g3 has " ++ show (length ends3) ++ " ends: " ++ show ends3)
-     mapM_ print [ spLength 0 n g3 | n <- ends3 ]
-
-     -- print (flood maze (entrance maze,keys maze))
-     -- print sptest
-     -- let g = build maze
-     -- prettyPrint g
-     -- print (spLength 0 9 g)
-
-sptest = sp 1 4 g >>= (traverse (`L.lookup` nodes))
+findOneSolution :: Maze -> Point -> [(Point,Int)]
+findOneSolution m (pos,miss) =
+  case next of
+    (_,[]) -> [step]
+    _      -> step : findOneSolution m next
   where
-    g = mkGraph nodes edges :: Gr Char Int
-    nodes = [(1,'a'),(2,'b'),(3,'c'),(4,'d')] :: [LNode Char]
-    edges = [(1,2,100),(1,3,1),(3,2,98),(3,4,200),(2,4,0)] :: [LEdge Int]
-
-
-
-type GrNode = LNode Point
-type GrEdge = LEdge Int
-
-type SearchGraph = Gr Point Int
-
-build :: Maze -> SearchGraph
-build m = go (mkGraph [(0,begin)] []) [(0,begin)]
-  where
-    begin = (entrance m,keys m) :: Point
-
-    go g [] = g
-    go g frontier = go g' frontier'
-      where
-        (g',frontier') = L.foldl' go' (g,[]) frontier
-
-        go' (g,acc) (l,p) = (g'',acc ++ [ (n,q) | (n,(q,_)) <- zip ids qds ])
-          where
-            qds = flood m p
-            ids = newNodes (length qds) g
-
-            g'  = L.foldl' (flip insNode) g  [ (n,q)   | (n,(q,_)) <- zip ids qds ]
-            g'' = L.foldl' (flip insEdge) g' [ (l,n,d) | (n,(_,d)) <- zip ids qds ]
-
-
-
-
-f :: Maze -> Gr Point Int
-f m = g''
-  where
-    begin = (entrance m,keys m) :: Point
-
-    g = mkGraph nodes edges :: Gr Point Int
-
-    nodes :: [GrNode]
-    nodes = [(0,begin)]
-
-    edges :: [GrEdge]
-    edges = []
-
-    pds = flood m begin :: [(Point,Int)] -- reachable points and distances
-
-    ids = newNodes (length pds) g
-
-    g'  = L.foldl' (flip insNode) g  [ (n,p)   | (n,(p,_)) <- zip ids pds ]
-    g'' = L.foldl' (flip insEdge) g' [ (0,n,d) | (n,(_,d)) <- zip ids pds ]
-
-
+    step@(next,_) = pick (flood m (pos,miss))
+    pick pts =
+      case (filter (\((c,_),_) -> (keyLocs m M.! c) `elem` miss) pts) of
+        [x] -> x
+        [_,x] -> x
+        (x:_) -> x
 
 type Coord = (Int,Int)
 type Tile = Char
 
-newtype Keys = Keys { fromKeys :: M.Map Coord Char }
-
-instance Show Keys where
-  show = L.sort . M.elems . fromKeys
+type Keys = [Char]
 
 data Maze = Maze
   { entrance :: Coord
   , keys     :: Keys
+  , keyLocs  :: M.Map Coord Char
   , doors    :: M.Map Coord Char
   , maze     :: S.Set Coord
-  } -- deriving ()
+  } deriving (Show)
 
 parse :: String -> Maze
-parse raw = Maze entrance (Keys keys) doors (S.fromList $ map fst maze)
+parse raw = Maze entrance keys keyLocs doors (S.fromList $ map fst maze)
   where
     tiles = [ ((x,y),t) | (y,xs) <- zip [0..] (lines raw), (x,t) <- zip [0..] xs ]
     [entrance] = [ p | (p,'@') <- maze ]
-    keys  = M.fromList [ (p,c) | (p,c) <- filter (isLower . snd) maze ]
-    doors = M.fromList [ (p,c) | (p,c) <- filter (isUpper . snd) maze ]
-    maze  = [ (p,t) | (p,t) <- tiles, t /= '#']
+    keyLocs = M.fromList [ (p,c) | (p,c) <- filter (isLower . snd) maze ]
+    doors   = M.fromList [ (p,c) | (p,c) <- filter (isUpper . snd) maze ]
+    keys    = M.elems keyLocs
+    maze    = [ (p,t) | (p,t) <- tiles, t /= '#']
 
 type Point = (Coord,Keys) -- (position, missing keys)
 
@@ -127,16 +61,15 @@ flood :: Maze -> Point -> [(Point,Int)]
 flood m@Maze{..} (start,startMissing) = go (S.singleton start) [start] 0
   where
     go seen frontier d
-      | null frontier = pts
+      | null frontier = []
       | otherwise     = pts ++ go seen' frontier' (d+1)
       where
-        candidates = [ q | p <- frontier, q <- adj m p
-                         , q `S.notMember` seen
-                         , q `M.notMember` doors || toLower (doors M.! q) `notElem` M.elems (fromKeys startMissing) ]
-        (found,free) = L.partition (`M.member` (fromKeys startMissing)) candidates
+        candidates = [ q | q <- (S.toList $ S.difference (S.fromList [ q | p <- frontier, q <- adj m p ]) seen)
+                         , q `M.notMember` doors || toLower (doors M.! q) `notElem` startMissing ]
+        (found,free) = L.partition (`M.member` (M.filter (\k -> k `elem` startMissing) keyLocs)) candidates
         seen' = S.union seen (S.fromList candidates)
         frontier' = free
-        pts = [ ((p,Keys $ M.delete p (fromKeys startMissing)),d+1) | p <- found ]
+        pts = [ ((p,L.delete (keyLocs M.! p) startMissing),d+1) | p <- found ]
 
 
 adj :: Maze -> Coord -> [Coord]
